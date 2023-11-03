eb4e614f253f21bc7c3a12ce8a3b38a70041de0c
Wesley Florence <wesleyflorence@Wesleys-Mac-Studio.local>
2023-11-02 18:32:02 -0700

    Github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -26,7 +26,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+    paths: {
+      // name of the repo for Github Pages deploy, remove if this changes
+      // https://kit.svelte.dev/docs/adapter-static
+      base: process.argv.includes('dev') ? '' : 'wesflorence-website'
+    }
 	},
 };
 
