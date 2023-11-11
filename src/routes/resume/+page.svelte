4c3726182468e45d44b8725c7f46d0d15850e097
Wesley Florence <wesleyflorence@Wesleys-Mac-Studio.local>
2023-11-10 16:49:49 -0800

    Adding resume pull from github

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from 'svelte';
+  let resumeHtml = ''; // This will hold the HTML content of your resume
+
+  onMount(async () => {
+    const response = await fetch('https://raw.githubusercontent.com/wesleyflorence/resume/main/wesley_florence_resume.html');
+    if (response.ok) {
+      resumeHtml = await response.text();
+    } else {
+      console.error('Failed to fetch the resume.');
+    }
+  });
+</script>
+
+<div class="resume-container">
+  {@html resumeHtml} <!-- Render the fetched HTML content -->
+</div>
+
+<style>
+</style>
