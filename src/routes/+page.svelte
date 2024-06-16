3824c69bb414d1a3e2c845c1c13bbebc5a944441
Wesley Florence <wesleyflorence@Wesleys-Mac-Studio.local>
2024-06-15 22:11:55 -0700

    Adding stupid big logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <svelte:head>
   <title>wesflorence</title>
 </svelte:head>
-<h1 class="text-4xl font-bold mb-2">Wes Florence</h1>
-<p>Hello. I'm a Software Developer living in Seattle, WA.</p>
-<div class="text-zinc-400">
-<a href="https://github.com/wesleyflorence" class="hover:text-zinc-50">Github</a>
-|
-<a href="https://www.linkedin.com/in/wesflorence" class="hover:text-zinc-50">LinkedIn</a>
+<video src="w-loop.webm" autoplay loop muted playsinline class="w-full h-auto"></video>
+<div class="flex flex-col items-center min-h-screen text-center">
+  <h1 class="text-4xl font-bold mb-2">Wes Florence</h1>
+  <p>Hello. I'm a Software Developer living in Seattle, WA.</p>
+  <div class="text-zinc-400">
+    <a href="https://github.com/wesleyflorence" class="hover:text-zinc-50">Github</a>
+    |
+    <a href="https://www.linkedin.com/in/wesflorence" class="hover:text-zinc-50">LinkedIn</a>
+  </div>
 </div>
